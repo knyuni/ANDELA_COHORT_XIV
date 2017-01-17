9dfaeb2922c5b8619c8ac38f4602a4a557d581ad
knyuni <ndugirekanyoni@gmail.com>
2017-01-17 14:18:31 +0300

    Assignment1
    Trial

---
+++ Day_0/primenumbers.py
@@ -0,0 +1,25 @@
+import unittest
+
+class InequalityTest(unittest.TestCase):
+    def test_Prime_Equal(self):
+        self.assertEqual(30, 2,3
+
+
+
+
+
+
+
+def is_prime(num):
+    for i in range(2,num):
+        if (num % i) == 0:
+            return False
+
+    return True
+
+def start():
+    num=int(raw_input('Enter a number: '))
+
+    prime_numbers (num)
+
+=
